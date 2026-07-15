ea1e16d86d07c03507120b9ab4d05e7a6e0d2cf2
abdelrahmanmohsen723-lang <abdelrahmanmohsen723@gmail.com>
2026-07-16 02:53:07 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,101 +1,3 @@
-// let generateBtn = document.querySelector(".generate");
-// let autoBtn = document.querySelector(".auto");
-// let stopBtn = document.querySelector(".stop");
-// let quoteDiv = document.querySelector(".quote-display");
-// let quoteId = document.querySelector(".quote-id");
-// let autoStatus = document.querySelector(".auto-status");
-// let intervalId;
-// generateBtn.onclick = generateQuote;
-// autoBtn.onclick = autoGenerate;
-// stopBtn.onclick = stopGenerate;
-
-// async function getQuotes() {
-//     const response = await fetch("quotes.json");
-//     const data = await response.json();
-//     return data;
-// }
-
-// async function generateQuote() {
-//     const quotes = await getQuotes();
-//     const quote = quotes[Math.floor(Math.random() * quotes.length)];
-//     quoteDiv.innerHTML = quote.text;
-//     quoteId.innerHTML = quote.id;
-// }
-// function autoGenerate() {
-//     intervalId = setInterval(generateQuote, 2000);
-//     autoStatus.innerHTML = "Auto : ON ";
-// }
-// function stopGenerate() {
-//     clearInterval(intervalId);
-//     autoStatus.innerHTML = "";
-// }
-//=================================RELODE===========================//
-// let generateBtn = document.querySelector(".generate");
-// let autoBtn = document.querySelector(".auto");
-// let stopBtn = document.querySelector(".stop");
-// let quoteDiv = document.querySelector(".quote-display");
-// let quoteId = document.querySelector(".quote-id");
-// let autoStatus = document.querySelector(".auto-status");
-// let intervalId;
-// generateBtn.onclick = generateQuote;
-// autoBtn.onclick = autoGenerate;
-// stopBtn.onclick = stopGenerate;
-
-// async function getQuotes() {
-//     let response = await fetch("quotes.json");
-//     let data = await response.json();
-//     return data;
-// }
-// async function generateQuote() {
-//     let quotes = await getQuotes();
-//   let quote = quotes[Math.floor(Math.random() * quotes.length)];
-//     quoteDiv.innerHTML = quote.text;
-//     quoteId.innerHTML = quote.id;
-// }
-// function autoGenerate() {
-//     intervalId = setInterval(generateQuote,2000)
-//     autoStatus.innerHTML = 'Auto : ON '
-// }
-// function stopGenerate() {
-//     clearInterval(intervalId)
-//     autoStatus.innerHTML = ''
-// }
-//===========================againe===============================//
-// let generateBtn = document.querySelector(".generate");
-// let autoBtn = document.querySelector(".auto");
-// let stopBtn = document.querySelector(".stop");
-// let quoteDiv = document.querySelector(".quote-display");
-// let quoteId = document.querySelector(".quote-id");
-// let autoStatus = document.querySelector(".auto-status");
-// let intervalId;
-
-// generateBtn.onclick = generateQuote;
-// autoBtn.onclick = autoGenerate;
-// stopBtn.onclick = stopGenerate;
-
-// async function getQuotes() {
-//     const response = await fetch("quotes.json");
-//     const data = await response.json();
-//     return data;
-// }
-
-// async function generateQuote() {
-//     const quotes = await getQuotes();
-//     const quote = quotes[Math.floor(Math.random() * quotes.length)];
-//     quoteDiv.innerHTML = quote.text;
-//     quoteId.innerHTML = quote.id;
-// }
-
-// function autoGenerate() {
-//     intervalId = setInterval(generateQuote, 2000);
-//     autoStatus.innerHTML = "Auto ON";
-// }
-
-// function stopGenerate() {
-//     clearInterval(intervalId);
-//     autoStatus.innerHTML = "";
-// }
-//===========================================againe========================================//
 let generateBtn = document.querySelector(".generate");
 let autoBtn = document.querySelector(".auto");
 let stopBtn = document.querySelector(".stop");
@@ -130,7 +32,7 @@ function stopGenerate() {
     clearInterval(intervalId);
     autoStatus.innerHTML = " ";
 }
-//=======================================againe========================================================//
+
 
 
 
